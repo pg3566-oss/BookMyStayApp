2a1ed3863587e301baf778afbfe389aaa91cd851
pg3566-oss <pg3566@srmist.edu.in>
2026-03-25 09:12:45 +0530

    pg3566-oss ADDED UC2

--- App/src/SuiteRoom.java
+++ App/src/SuiteRoom.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+/**
+ * ===============================================================
+ * CLASS - SuiteRoom
+ * ===============================================================
+ *
+ * Represents a suite room in the hotel.
+ *
+ * @version 2.1
+ */
 
-public class SuiteRoom {
+public class SuiteRoom extends Room {
+
+    /**
+     * Initializes a SuiteRoom with predefined attributes.
+     */
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
 }
