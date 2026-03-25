1a1d34a65fee5de9f4fda2cb3abb6f95f5b8ecbc
pg3566-oss <pg3566@srmist.edu.in>
2026-03-25 09:04:52 +0530

    pg3566-oss ADDED UC2

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,34 +1,7 @@
-/**
- * UC1 - Application Entry Point for Book My Stay App
- *
- * This class demonstrates the starting point of a Java application.
- * It prints a welcome message, application name, and version details.
- *
- * @author Pallavi
- * @version 1.0
- */
+
 public class BookMyStayApp {
 
-    /**
-     * Main method - Entry point of the application
-     * JVM starts execution from here.
-     *
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
 
-        // Welcome Message
-        System.out.println("======================================");
-        System.out.println("   Welcome to Book My Stay App 🏨");
-        System.out.println("======================================");
-
-        // Application Details
-        System.out.println("Application Name : Hotel Booking Management System");
-        System.out.println("Version          : v1.0");
-
-        // Closing Message
-        System.out.println("--------------------------------------");
-        System.out.println("Application started successfully!");
-        System.out.println("--------------------------------------");
     }
 }
